551f11da97cb016ce2e77240f6a055408c9f322d
rcmilan <13418519+rcmilan@users.noreply.github.com>
2023-01-20 09:47:17 -0300

    cleanup

--- src/pages/logic/index.tsx
+++ src/pages/logic/index.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import LogicConnector from "../../components/logicConnector";
 import "./logic.css";
 
+const wrapperStyle: React.CSSProperties = {
+  padding: "10px",
+};
+
 const _and = (p: boolean, q: boolean) => (p && q).toString();
 const _or = (p: boolean, q: boolean) => (p || q).toString();
 const _not = (p: boolean) => (!p).toString();
@@ -14,50 +18,44 @@ const cases = [
 ];
 
 const andTable = (
-  <div>
-    <p>AND</p>
-    <table>
-      <thead>
-        <tr>
-          <th>p</th>
-          <th>q</th>
-          <th>p AND q</th>
-        </tr>
-      </thead>
-      <tbody>
-        {cases.map((v, i) => (
-          <LogicConnector key={i} p={v[0]} q={v[1]} conn={_and} />
-        ))}
-      </tbody>
-    </table>
-  </div>
+  <table>
+    <thead>
+      <tr>
+        <th>p</th>
+        <th>q</th>
+        <th>p AND q</th>
+      </tr>
+    </thead>
+    <tbody>
+      {cases.map((v, i) => (
+        <LogicConnector key={i} p={v[0]} q={v[1]} conn={_and} />
+      ))}
+    </tbody>
+  </table>
 );
 
 const orTable = (
-  <div>
-    <p>OR</p>
-    <table>
-      <thead>
-        <tr>
-          <th>p</th>
-          <th>q</th>
-          <th>p OR q</th>
-        </tr>
-      </thead>
-      <tbody>
-        {cases.map((v, i) => (
-          <LogicConnector key={i} p={v[0]} q={v[1]} conn={_or} />
-        ))}
-      </tbody>
-    </table>
-  </div>
+  <table>
+    <thead>
+      <tr>
+        <th>p</th>
+        <th>q</th>
+        <th>p OR q</th>
+      </tr>
+    </thead>
+    <tbody>
+      {cases.map((v, i) => (
+        <LogicConnector key={i} p={v[0]} q={v[1]} conn={_or} />
+      ))}
+    </tbody>
+  </table>
 );
 
 function Logic() {
   return (
     <>
-      {andTable}
-      {orTable}
+      <div style={wrapperStyle}>{andTable}</div>
+      <div style={wrapperStyle}>{orTable}</div>
     </>
   );
 }
