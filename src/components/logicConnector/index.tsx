a493a932fb6ab3f80cb2cc84934f4469d81874dc
rcmilan <13418519+rcmilan@users.noreply.github.com>
2023-01-20 09:41:44 -0300

    tabelas AND e OR

---
+++ src/components/logicConnector/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface LogicParams {
+  p: boolean;
+  q: boolean;
+  conn: (p: boolean, q: boolean) => string;
+}
+
+const LogicConnector = ({ p, q, conn }: LogicParams) => {
+  return (
+    <tr>
+      <td>{p.toString()}</td>
+      <td>{q.toString()}</td>
+      <td>{conn(p, q)}</td>
+    </tr>
+  );
+};
+
+export default LogicConnector;
--- src/pages/logic/index.tsx
+++ src/pages/logic/index.tsx
@@ -1,10 +1,64 @@
 import React from "react";
+import LogicConnector from "../../components/logicConnector";
+import "./logic.css";
+
+const _and = (p: boolean, q: boolean) => (p && q).toString();
+const _or = (p: boolean, q: boolean) => (p || q).toString();
+const _not = (p: boolean) => (!p).toString();
+
+const cases = [
+  [true, true],
+  [true, false],
+  [false, true],
+  [false, false],
+];
+
+const andTable = (
+  <div>
+    <p>AND</p>
+    <table>
+      <thead>
+        <tr>
+          <th>p</th>
+          <th>q</th>
+          <th>p AND q</th>
+        </tr>
+      </thead>
+      <tbody>
+        {cases.map((v, i) => (
+          <LogicConnector key={i} p={v[0]} q={v[1]} conn={_and} />
+        ))}
+      </tbody>
+    </table>
+  </div>
+);
+
+const orTable = (
+  <div>
+    <p>OR</p>
+    <table>
+      <thead>
+        <tr>
+          <th>p</th>
+          <th>q</th>
+          <th>p OR q</th>
+        </tr>
+      </thead>
+      <tbody>
+        {cases.map((v, i) => (
+          <LogicConnector key={i} p={v[0]} q={v[1]} conn={_or} />
+        ))}
+      </tbody>
+    </table>
+  </div>
+);
 
 function Logic() {
   return (
-    <div>
-      <>Tabela verdade</>
-    </div>
+    <>
+      {andTable}
+      {orTable}
+    </>
   );
 }
 
